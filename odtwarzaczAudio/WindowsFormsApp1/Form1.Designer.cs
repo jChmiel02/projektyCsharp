5683ec6e0933495f52d705b8e64e09f4a9874893
JakubChmiel <xloriksx123@wp.pl>
2025-02-20 15:34:14 +0100

    Dodanie projektu odtwarzaczaAudio

---
+++ odtwarzaczAudio/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.btnDelFile = new System.Windows.Forms.Button();
+            this.lstPlaylist = new System.Windows.Forms.ListBox();
+            this.lblCurrentFile = new System.Windows.Forms.Label();
+            this.btnRewind = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPlay.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnPlay.Location = new System.Drawing.Point(12, 50);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPause.Location = new System.Drawing.Point(114, 50);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStop.Location = new System.Drawing.Point(206, 50);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnAddFile.Location = new System.Drawing.Point(563, 100);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFile.TabIndex = 3;
+            this.btnAddFile.Text = "Add File";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.btnAddFile_Click);
+            // 
+            // btnDelFile
+            // 
+            this.btnDelFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDelFile.Location = new System.Drawing.Point(563, 177);
+            this.btnDelFile.Name = "btnDelFile";
+            this.btnDelFile.Size = new System.Drawing.Size(75, 23);
+            this.btnDelFile.TabIndex = 4;
+            this.btnDelFile.Text = "Del File";
+            this.btnDelFile.UseVisualStyleBackColor = true;
+            this.btnDelFile.Click += new System.EventHandler(this.btnDelFile_Click);
+            // 
+            // lstPlaylist
+            // 
+            this.lstPlaylist.BackColor = System.Drawing.SystemColors.InfoText;
+            this.lstPlaylist.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lstPlaylist.ForeColor = System.Drawing.SystemColors.Info;
+            this.lstPlaylist.FormattingEnabled = true;
+            this.lstPlaylist.ItemHeight = 16;
+            this.lstPlaylist.Location = new System.Drawing.Point(12, 100);
+            this.lstPlaylist.Name = "lstPlaylist";
+            this.lstPlaylist.Size = new System.Drawing.Size(525, 100);
+            this.lstPlaylist.TabIndex = 5;
+            this.lstPlaylist.Click += new System.EventHandler(this.lstPlaylist_DoubleClick);
+            // 
+            // lblCurrentFile
+            // 
+            this.lblCurrentFile.AutoSize = true;
+            this.lblCurrentFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCurrentFile.Location = new System.Drawing.Point(12, 203);
+            this.lblCurrentFile.Name = "lblCurrentFile";
+            this.lblCurrentFile.Size = new System.Drawing.Size(169, 22);
+            this.lblCurrentFile.TabIndex = 6;
+            this.lblCurrentFile.Text = "Nie wybrano pliku";
+            this.lblCurrentFile.Click += new System.EventHandler(this.lblCurrentFile_Click);
+            // 
+            // btnRewind
+            // 
+            this.btnRewind.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnRewind.Location = new System.Drawing.Point(12, 251);
+            this.btnRewind.Name = "btnRewind";
+            this.btnRewind.Size = new System.Drawing.Size(75, 23);
+            this.btnRewind.TabIndex = 7;
+            this.btnRewind.Text = "<-5s";
+            this.btnRewind.UseVisualStyleBackColor = true;
+            this.btnRewind.Click += new System.EventHandler(this.btnRewind_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnForward.Location = new System.Drawing.Point(462, 251);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 23);
+            this.btnForward.TabIndex = 8;
+            this.btnForward.Text = "5s ->";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(650, 300);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnRewind);
+            this.Controls.Add(this.lblCurrentFile);
+            this.Controls.Add(this.lstPlaylist);
+            this.Controls.Add(this.btnDelFile);
+            this.Controls.Add(this.btnAddFile);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Form1";
+            this.Text = "Music Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.Button btnDelFile;
+        private System.Windows.Forms.ListBox lstPlaylist;
+        private System.Windows.Forms.Label lblCurrentFile;
+        private System.Windows.Forms.Button btnRewind;
+        private System.Windows.Forms.Button btnForward;
+    }
+}
---
+++ odtwarzaczAudio/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private WindowsMediaPlayer player;
+        public Form1()
+        {
+            InitializeComponent();
+            player = new WindowsMediaPlayer();
+            lblCurrentFile.Text = "Nie wybrano żadnego pliku";
+            player.PlayStateChange += Player_PlayStateChange;
+
+        }
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (lstPlaylist.SelectedItem != null)
+            {
+                player.URL = lstPlaylist.SelectedItem.ToString();
+                player.controls.play();
+                lblCurrentFile.Text = $"Aktualnie: {System.IO.Path.GetFileName(player.URL)}";
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano żadnego pliku do odtworzenia.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (player.playState == WMPPlayState.wmppsPlaying)
+            {
+                player.controls.pause();
+                lblCurrentFile.Text = "Wstrzymano";
+            }
+            else if (player.playState == WMPPlayState.wmppsPaused)
+            {
+                player.controls.play();
+                lblCurrentFile.Text = $"Aktualnie: {System.IO.Path.GetFileName(player.URL)}";
+            }
+            else
+            {
+                MessageBox.Show("Nie odtwarzany jest żaden plik.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (player.playState == WMPPlayState.wmppsPlaying || player.playState == WMPPlayState.wmppsPaused)
+            {
+                player.controls.stop();
+                lblCurrentFile.Text = "Zatrzymano";
+            }
+            else
+            {
+                MessageBox.Show("Nie odtwarzany jest żaden plik.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnAddFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Pliki audio|*.mp3;*.wav;*.wma|Wszystkie pliki|*.*";
+                ofd.Title = "Wybierz plik audio";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    lstPlaylist.Items.Add(ofd.FileName);
+                }
+            }
+        }
+        private void btnDelFile_Click(object sender, EventArgs e)
+        {
+            if (lstPlaylist.SelectedItem != null)
+            {
+                lstPlaylist.Items.Remove(lstPlaylist.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano żadnego pliku do usunięcia.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void lstPlaylist_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstPlaylist.SelectedItem != null)
+            {
+                player.URL = lstPlaylist.SelectedItem.ToString();
+                player.controls.play();
+                lblCurrentFile.Text = $"Aktualnie: {System.IO.Path.GetFileName(player.URL)}";
+            }
+        }
+        private void Player_PlayStateChange(int newState)
+        {
+            if ((WMPPlayState)newState == WMPPlayState.wmppsMediaEnded)
+            {
+                PlayNextFile();
+            }
+        }
+
+
+        private async void PlayNextFile()
+        {
+            if (lstPlaylist.SelectedIndex < lstPlaylist.Items.Count - 1)
+            {
+                lstPlaylist.SelectedIndex++;
+                string nextFile = lstPlaylist.SelectedItem.ToString();
+                player.URL = nextFile;
+                lblCurrentFile.Text = $"Aktualnie: {System.IO.Path.GetFileName(nextFile)}";
+                await Task.Delay(100);
+                player.controls.play();
+            }
+            else
+            {
+                player.controls.stop();
+                lblCurrentFile.Text = "Odtwarzanie zakończone";
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (player.playState == WMPPlayState.wmppsPlaying || player.playState == WMPPlayState.wmppsPaused)
+            {
+                player.controls.currentPosition += 5;
+            }
+            else
+            {
+                MessageBox.Show("Nie odtwarzany jest żaden plik.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnRewind_Click(object sender, EventArgs e)
+        {
+            if (player.playState == WMPPlayState.wmppsPlaying || player.playState == WMPPlayState.wmppsPaused)
+            {
+                player.controls.currentPosition -= 5;
+
+                if (player.controls.currentPosition < 0)
+                {
+                    player.controls.currentPosition = 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie odtwarzany jest żaden plik.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void lblCurrentFile_Click(object sender, EventArgs e) { }
+    }
+}
